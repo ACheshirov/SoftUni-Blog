39b6d0ed72aad63d8537a27551455567bc854282
Aleksandar Cheshirov <alex.cheshirov@gmail.com>
2017-03-10 23:10:30 +0200

    Added links for registered users and admins

--- application/views/layouts/baseLayout.php
+++ application/views/layouts/baseLayout.php
@@ -28,23 +28,29 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                 <li class="active"><a href="<?=site_url("")?>">Начало</a></li>
                 <li><a href="#about">About</a></li>
                 <li><a href="#contact">Contact</a></li>
-                <li class="dropdown">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-                    <ul class="dropdown-menu">
-                        <li><a href="#">Action</a></li>
-                        <li><a href="#">Another action</a></li>
-                        <li><a href="#">Something else here</a></li>
-                        <li role="separator" class="divider"></li>
-                        <li class="dropdown-header">Nav header</li>
-                        <li><a href="#">Separated link</a></li>
-                        <li><a href="#">One more separated link</a></li>
-                    </ul>
-                </li>
             </ul>
             <ul class="nav navbar-nav navbar-right">
-                <li><a href="../navbar/">Default</a></li>
-                <li class="active"><a href="./">Static top <span class="sr-only">(current)</span></a></li>
-                <li><a href="../navbar-fixed-top/">Fixed top</a></li>
+                <?php if ($_isLogged) : ?>
+                    <?php if ($_isAdmin) : ?>
+                        <li class="dropdown">
+                            <a class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Администратор <span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">Action</a></li>
+                                <li><a href="#">Another action</a></li>
+                                <li><a href="#">Something else here</a></li>
+                                <li role="separator" class="divider"></li>
+                                <li class="dropdown-header">Nav header</li>
+                                <li><a href="#">Separated link</a></li>
+                                <li><a href="#">One more separated link</a></li>
+                            </ul>
+                        </li>
+                    <?php endif ?>
+                    <li><a href="<?=site_url("logout")?>">Изход</a></li>
+                <?php else : ?>
+                    <li><a href="<?=site_url("login")?>">Вход</a></li>
+                    <li><a href="<?=site_url("register")?>">Регистрация</a></li>
+                <?php endif ?>
+
             </ul>
         </div>
     </div>
