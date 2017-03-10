3a4ba57bf191a08b77e2d21ff4b020a5d3e37e5e
Aleksandar Cheshirov <alex.cheshirov@gmail.com>
2017-03-10 23:13:18 +0200

    Integrated approving and deleting of comments

--- application/models/Comments.php
+++ application/models/Comments.php
@@ -13,10 +13,59 @@ class Comments extends CI_Model
         return null;
     }
 
-    public function postComment($id, $author, $email, $description, $ip, $approved = false) {
+    /**
+     * @param $id
+     * @return bool
+     */
+    public function deleteComment($id) {
+        if (is_array($id)) {
+            $id = array_filter($id, function ($v) {
+                return is_numeric($v);
+            });
+            if (!count($id)) return false;
+        }
+        elseif (is_numeric($id))
+            $id = array($id);
+        else return false;
+
+        $this->db->where_in('id', $id);
+        return $this->db->delete('comments');
+    }
+
+    /**
+     * @param $id
+     * @return bool
+     */
+    public function approveComment($id) {
+        if (is_array($id)) {
+            $id = array_filter($id, function ($v) {
+                return is_numeric($v);
+            });
+            if (!count($id)) return false;
+        }
+        elseif (is_numeric($id))
+            $id = array($id);
+        else return false;
+
+        $this->db->set("approved", 1);
+        $this->db->where_in('id', $id);
+        return $this->db->update('comments');
+    }
+
+    /**
+     * @param $id
+     * @param $author
+     * @param $email
+     * @param $description
+     * @param $ip
+     * @param int $user_id
+     * @param bool $approved
+     */
+    public function postComment($id, $author, $email, $description, $ip, $user_id = 0, $approved = false) {
         if (is_numeric($id)) {
             $data = array(
                 'post_id' => $id,
+                'user_id' => $user_id,
                 'author' => $author,
                 'description' => $description,
                 'dateCreate' => date("Y-m-d H:i:s"),
@@ -27,7 +76,7 @@ class Comments extends CI_Model
             if ($email !== null)
                 $data['email'] = $email;
 
-            $this->db->insert('comments', $data);
+            return $this->db->insert('comments', $data);
         }
     }
 }
