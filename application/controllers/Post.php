0a501572974e2b9d037e12f35c1c477ae211bc2a
Aleksandar Cheshirov <alex.cheshirov@gmail.com>
2017-03-10 23:11:41 +0200

    Integrated approving and deleting of comments

--- application/controllers/Post.php
+++ application/controllers/Post.php
@@ -9,12 +9,17 @@ class Post extends MY_Controller
         if ($post == "index")
             $isPostExists = false;
 
+        $this->load->model("Posts");
+
+        if ($post == "new") {
+            $this->show("pages/posts/new");
+            return;
+        }
+
         $exPost = explode("-", $post);
         $idPost = end($exPost);
 
         if (is_numeric($idPost)) {
-            $this->load->model("Posts");
-
             $postInfo = $this->Posts->getPost($idPost);
             if (!isset($postInfo)) {
                 $isPostExists = false;
@@ -36,18 +41,36 @@ class Post extends MY_Controller
         if ($this->input->post("post") !== null) {
             $this->load->library('form_validation');
 
-            $this->form_validation->set_rules(
-                'author', 'Автор',
-                'trim|required|min_length[3]|max_length[30]',
-                array(
-                    'required' => 'Не сте оказали кой е автора.',
-                    'min_length' => 'Името ви трябва да съдържа минимум 3 символа.',
-                    'max_length' => 'Името ви трябва да съдържа максимум 30 символа.',
-                )
-            );
-            $this->form_validation->set_rules('email', 'E-mail адрес', 'trim|valid_email', array(
-                'valid_email' => 'Не сте въвели валиден E-mail адрес.'
-            ));
+            $userId = $this->isLogged();
+            $username = $this->input->post("author");
+            $email = $this->input->post("email");
+
+            $approved = false;
+
+            if (!$userId) {
+                $this->form_validation->set_rules(
+                    'author', 'Автор',
+                    'trim|required|min_length[3]|max_length[30]',
+                    array(
+                        'required' => 'Не сте оказали кой е автора.',
+                        'min_length' => 'Името ви трябва да съдържа минимум 3 символа.',
+                        'max_length' => 'Името ви трябва да съдържа максимум 30 символа.',
+                    )
+                );
+                $this->form_validation->set_rules('email', 'E-mail адрес', 'trim|valid_email', array(
+                    'valid_email' => 'Не сте въвели валиден E-mail адрес.'
+                ));
+            } else {
+                $this->load->model("Users");
+                $userInfo = $this->Users->getUserById($userId);
+
+                if ($userInfo['trusted'] || $userInfo['admin'])
+                    $approved = true;
+
+                $username = $userInfo['username'];
+                $email = $userInfo['email'];
+            }
+
             $this->form_validation->set_rules('description', 'Съобщение', 'trim|required|min_length[5]|max_length[500]', array(
                 'required' => 'Не сте написали съобщение.',
                 'min_length' => 'Съобщението Ви трябва да съдържа минимум 5 символа.',
@@ -55,26 +78,27 @@ class Post extends MY_Controller
             ));
 
             if ($this->form_validation->run() !== FALSE) {
-                $this->Comments->postComment($idPost, $this->input->post("author"), $this->input->post("email"), $this->input->post("description"), $this->input->ip_address());
+                $this->Comments->postComment($idPost, $username, $email, $this->input->post("description"), $this->input->ip_address(), $userId, $approved);
                 redirect(uri_string());
                 return;
             } else
                 $data['commentResult'] = current($this->form_validation->error_array());
         }
 
-        $this->deleteComment($this->input->get("delComment"));
-
-        $data['isAdmin'] = $this->isAdmin();
-        $data['isLogged'] = $this->isLogged();
+        $this->approve_delete_comments();
 
         $data['comments'] = $this->Comments->getComments($idPost, $this->isAdmin());
 
-        $this->show("post", $data);
+        $this->show("pages/posts/details", $data);
     }
 
-    public function deleteComment($delComment) {
-        if ($delComment !== null && $this->isAdmin()) {
+    public function approve_delete_comments() {
+        if (($this->input->get("delComment") !== null || $this->input->get("approveComment") !== null) && $this->isAdmin() && $this->isTokenValid($this->input->get("token"))) {
+            if ($this->input->get("delComment") !== null)
+                $this->Comments->deleteComment($this->input->get("delComment"));
 
+            if ($this->input->get("approveComment") !== null)
+                $this->Comments->approveComment($this->input->get("approveComment"));
         }
     }
 }
