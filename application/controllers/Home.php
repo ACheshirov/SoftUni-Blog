a1658afcfd5858b94b946f4de3dd0968446d022f
Aleksandar Cheshirov <alex.cheshirov@gmail.com>
2017-03-11 03:04:34 +0200

    New model for post listing by category

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -2,14 +2,34 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Home extends MY_Controller {
-	public function index()
-	{
-        $this->load->model("Posts");
+    public function __construct() {
+        parent::__construct();
 
+        $this->load->model("Posts");
+    }
+
+    public function index()
+	{
         $data = array(
             "posts" => $this->Posts->getPosts()
         );
 
 		$this->show("pages/home", $data);
 	}
+
+	public function category($category) {
+        $exCategory = explode("-", $category);
+        $idCategory = end($exCategory);
+
+        if (!is_numeric($idCategory)) {
+            $this->show_404();
+            return;
+        }
+
+        $data = array(
+            "posts" => $this->Posts->getPostsByCategory($idCategory)
+        );
+
+        $this->show("pages/home", $data);
+    }
 }
