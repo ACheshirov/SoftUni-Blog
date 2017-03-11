832b924e5052c7b9c213ad9d93a4b82f451ca7f2
Aleksandar Cheshirov <alex.cheshirov@gmail.com>
2017-03-11 03:37:45 +0200

    Nothing special. :P

--- application/views/pages/posts/details.php
+++ application/views/pages/posts/details.php
@@ -10,9 +10,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <section class="panel">
     <h4>Коментари:</h4>
 
-    <?php
-    if (isset($commentResult) && $commentResult !== null) : ?>
+    <?php if (isset($commentResult) && $commentResult !== null) : ?>
         <div class="alert alert-danger"><?=$commentResult?></div>
+    <?php endif; ?>
+
+    <?php if ($this->session->flashdata('newComment') !== null) : ?>
+        <div class="alert alert-success"><?=$this->session->flashdata('newComment')?></div>
     <?php endif; ?>
 
     <?=form_open()?>
@@ -36,7 +39,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
     <hr />
     <?php foreach ($comments as $comment) : ?>
     <div class="comment">
-        <i class="fa fa-user-o"></i> <span class="author"><?=htmlspecialchars($comment['author'])?></span>
+        <i class="fa fa-user-o"></i> <span class="author"><?=($comment['user_id']) ? '<a href="'.site_url("profile/user/".$comment['author']).'">' : ''?><?=htmlspecialchars($comment['author'])?><?=($comment['user_id']) ? '</a>' : ''?></span>
         <span class="options"><?=(!$comment['approved'] && $_isAdmin) ? '[<a href="?approveComment='.$comment['id'].'&token='.$_token.'">Одобри</a>] ' : ''?><?=($_isAdmin) ? '[<a href="?delComment='.$comment['id'].'&token='.$_token.'" onclick="return confirm(\'Сигурни ли сте?\')">Изтрий</a>]' : ''?></span>
         <p><?=nl2br(htmlspecialchars($comment['description']));?></p>
         &nbsp;<span class="date"><i class="fa fa-clock-o"></i> <?=date('H:i d.m.Y', strtotime($comment['dateCreate']))?></span>
